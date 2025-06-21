95d8bd9df2369864dbd867098744b8324af886ea
Samuel Salazar <samsalra.007@gmail.com>
2025-06-20 20:53:34 -0600

    Fixing queries

--- data/models/models.sql
+++ data/models/models.sql
@@ -1,41 +1,46 @@
 DROP DATABASE IF EXISTS wolfbear_os;
-
 CREATE DATABASE wolfbear_os;
 
-CREATE USER 'wolfbear_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'welcome';
-GRANT ALL PRIVILEGES ON wolfbear_os.* TO 'wolfbear_user'@'localhost' WITH GRANT OPTION;
+CREATE USER 'wolfbear_user'@'localhost' IDENTIFIED BY 'welcome';
+GRANT ALL PRIVILEGES ON wolfbear_os.* TO 'wolfbear_user'@'localhost';
+
 FLUSH PRIVILEGES;
 
 USE wolfbear_os;
 
+-- Tabla base de jugadores
 CREATE TABLE players (
     id INT NOT NULL AUTO_INCREMENT,
-    
+
     username VARCHAR(20),
     password VARCHAR(200),
 
     PRIMARY KEY (id)
-) Engine = INNODB;
+) ENGINE = InnoDB;
 
+-- Tabla de perfiles (historial completo)
 CREATE TABLE profiles (
     id INT NOT NULL AUTO_INCREMENT,
     player_id INT NOT NULL,
 
-    creation_date datetime
-    modification_date datetime
+    creation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    modification_date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
-    names varchar(30),
-    lastname varchar(60),
-
-    email varchar(50),
+    names VARCHAR(30),
+    lastname VARCHAR(60),
+    email VARCHAR(50),
 
     PRIMARY KEY (id),
-    PRIMARY KEY (player_id),
+    FOREIGN KEY (player_id) REFERENCES players(id)
+) ENGINE = InnoDB;
 
-    FOREIGN KEY (player_id) REFERENCES players (id)    
-) Engine = INNODB;
-
+-- Tabla que apunta al perfil más reciente
 CREATE TABLE profiles_latest (
+    id INT NOT NULL AUTO_INCREMENT,
     profile_id INT NOT NULL,
-    modification_date datetime NOT NULL
-) Engine = INNODB;
+
+    modification_date DATETIME NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (profile_id) REFERENCES profiles(id)
+) ENGINE = InnoDB;
